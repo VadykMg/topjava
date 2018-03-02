355530632f69d7c87a14d923b1c448e98492392d
VadykMg <magnat394@gmail.com>
2018-03-02 22:23:03 +0200

    Prepare to HW0

--- src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
@@ -3,11 +3,14 @@ package ru.javawebinar.topjava.util;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.MealWithExceed;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class MealsUtil {
     public static void main(String[] args) {
@@ -19,13 +22,16 @@ public class MealsUtil {
                 new Meal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
                 new Meal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510)
         );
-        getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
-//        .toLocalDate();
-//        .toLocalTime();
+        List<MealWithExceed> filteredWithExeed = getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
+        filteredWithExeed.forEach((m-> System.out.println(m.toString())));
+
     }
 
     public static List<MealWithExceed>  getFilteredWithExceeded(List<Meal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        // TODO return filtered list with correctly exceeded field
-        return null;
+        Map<LocalTime, Integer> caloriesSumByDay = mealList.stream().collect(Collectors.groupingBy(um -> um.getDateTime().toLocalTime(), Collectors.summingInt(Meal::getCalories)));
+        return mealList.stream()
+                .filter(um ->TimeUtil.isBetween(um.getDateTime().toLocalTime(), startTime, endTime))
+                .map(um->new MealWithExceed(um.getDateTime(),um.getDescription(),um.getCalories(),caloriesSumByDay.get(um.getDateTime().toLocalDate())>caloriesPerDay))
+                .collect(Collectors.toList());
     }
 }
